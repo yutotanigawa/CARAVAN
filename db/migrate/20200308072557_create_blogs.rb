5168e999e511b4fa883344dae2ab05157577bb46
yutotanigawa <61495631+yutotanigawa@users.noreply.github.com>
2020-03-08 16:29:10 +0900

    [finish]4-1

--- db/migrate/20200308072557_create_blogs.rb
+++ db/migrate/20200308072557_create_blogs.rb
@@ -1,6 +1,9 @@
 class CreateBlogs < ActiveRecord::Migration[5.2]
   def change
     create_table :blogs do |t|
+      t.string :title
+      t.string :category
+      t.text :body
 
       t.timestamps
     end
