1ebe77311876e4afa9d83126521b228077ef5e4a
yutotanigawa <61495631+yutotanigawa@users.noreply.github.com>
2020-03-08 17:20:01 +0900

    [finish]4-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -11,8 +11,18 @@ class BlogsController < ApplicationController
   end
 
   def create
+  	blog = Blog.new(blog_params)
+  	blog.save
+  	redirect_to blogs_path
+
   end
 
   def edit
   end
+
+  private
+  def blog_params
+  	params.require(:blog).permit(:title, :category, :body)
+  end
+
 end
